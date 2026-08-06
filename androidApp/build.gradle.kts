8f9543a42cc9348250edf038df26bcca917ada87
Thomas Bernard <thomas.bernard@ikmail.com>
2026-08-06 17:08:11 +0200

    chore: bump version 2026.08.1

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = 2
-        versionName = "2026.07.1"
+        versionName = "2026.08.1"
     }
     packaging {
         resources {
